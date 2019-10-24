0c9b95450883bf204b79e6f4dbbd45a292e162f3
100yang <1989152546@qq.com>
2019-10-24 20:03:24 +0800

    Alogrithm

---
+++ Leetcode/14. 最长公共前缀.cpp
@@ -0,0 +1,24 @@
+/*
+* @Author: 100yang
+* @Date:   2019-10-24 15:53:44
+* @Last Modified by:   100yang
+* @Last Modified time: 2019-10-24 16:00:19
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+	string longestCommonPrefix(vector<string>& strs) {
+		string ans = "";
+		
+	}
+} mm;
+int main() {
+	int n;
+	string str;
+	std::vector<string> v;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {cin >> str; v.push_back(str);}
+	cout << mm.longestCommonPrefix(v) << endl;
+	return 0;
+}
